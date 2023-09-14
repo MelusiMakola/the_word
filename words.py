b18ff2688b144bf4c1a264375c24492464020ad3
MelusiMakola <101884620+MelusiMakola@users.noreply.github.com>
2023-09-14 02:01:29 -0700

    Add files via upload

---
+++ words.py
@@ -0,0 +1,9 @@
+#I create a list of secret words so we have different words for every game
+secret_words = ["Intelligent",
+                 "Programming", 
+                 "Python"] 
+
+#I also create a list of clue and I make sure the index of every clue corresponds with the right word.
+clues = ["Another word for smart.",
+        "What do most programmers spend their time doing",
+        "The language used in this game's program"]
